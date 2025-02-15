4ddc83d41d44dcdeedd6154af5552429d9ab3554
Badam Neeraj Kumar <69072023+neeraj-badam@users.noreply.github.com>
2025-02-15 18:56:57 +0530

    Add files via upload

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -1,9 +1,9 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { login } from "../redux/userSlice";
 import { auth } from "../firebase";
 import { createUserWithEmailAndPassword } from "firebase/auth";
-import { useNavigate, Link } from "react-router-dom";
+import { useNavigate, Link, useLocation } from "react-router-dom";
 import "../index.css"; // Import CSS
 
 
@@ -15,6 +15,19 @@ function Register() {
   const [address, setAddress] = useState("");
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const location = useLocation();
+
+
+  // ✅ Prefill Email & Name if redirected from Google Login
+  useEffect(() => {
+    if (location.state) {
+      if(  location.state.email ){
+        alert( "Please Create an account first" );
+      }
+      setEmail(location.state.email || "");
+      setName(location.state.name || "");
+    }
+  }, [location]);
 
   const handleRegister = async () => {
     try {
